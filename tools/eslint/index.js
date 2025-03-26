453fa4e8637081f813a8bbc5df30eba8072eb863
zeroqs <smthq@bk.ru>
2025-03-26 03:24:04 +0300

    fix: specify effector rules directly

--- tools/eslint/index.js
+++ tools/eslint/index.js
@@ -1,5 +1,5 @@
 import antfu from '@antfu/eslint-config';
-import { fixupConfigRules, fixupPluginRules } from '@eslint/compat';
+import { fixupPluginRules } from '@eslint/compat';
 import pluginNext from '@next/eslint-plugin-next';
 import * as effectorRule from 'eslint-plugin-effector';
 import pluginJsxA11y from 'eslint-plugin-jsx-a11y';
@@ -18,7 +18,19 @@ export const eslint = (
         effector: fixupPluginRules(effectorRule)
       },
       name: 'effector',
-      rules: fixupConfigRules(effectorRule.default.configs.recommended.rules)
+      rules: {
+        'effector/enforce-store-naming-convention': 'error',
+        'effector/enforce-effect-naming-convention': 'error',
+        'effector/no-getState': 'error',
+        'effector/no-useless-methods': 'error',
+        'effector/no-unnecessary-duplication': 'warn',
+        'effector/prefer-sample-over-forward-with-mapping': 'warn',
+        'effector/no-ambiguity-target': 'warn',
+        'effector/no-watch': 'warn',
+        'effector/no-unnecessary-combination': 'warn',
+        'effector/no-duplicate-on': 'error',
+        'effector/keep-options-order': 'warn'
+      }
     });
   }
 
